a8e3657c77568cd9b4c8c9aa0ce0c167f304d26c
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-06-10 21:28:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,9 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+function addElementToBeginningOfArray (arr, elem) {
+  return [elem, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, elem) {
+  arr.unshift(elem);
+  return arr;
+}
+
